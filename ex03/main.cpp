f8c0dcad6e0c554877e002309fd55f23cfe6d9f0
Nick Talmon <ntalmon@2-B-1.42heilbronn.de>
2024-08-14 11:48:12 +0200

    ready for eval

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,37 +6,34 @@
 /*   By: ntalmon <ntalmon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 12:09:47 by ntalmon           #+#    #+#             */
-/*   Updated: 2024/08/13 17:43:23 by ntalmon          ###   ########.fr       */
+/*   Updated: 2024/08/14 11:00:51 by ntalmon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
-#include "ScavTrap.hpp"
-#include "FragTrap.hpp"
 #include "DiamondTrap.hpp"
 
 int	main(void)
 {
-	// ClapTrap	ClapTrap("ClapTrap");
-	// ScavTrap	ScavTrap("ScavTrap");
-	// FragTrap	FragTrap("FragTrap");
+	ClapTrap	ClapTrap("ClapTrap");
+	ScavTrap	ScavTrap("ScavTrap");
+	FragTrap	FragTrap("FragTrap");
 	DiamondTrap	DiamondTrap("Nick");
 
-	// ClapTrap.attack("target");
-	// ClapTrap.takeDamage(10);
-	// ClapTrap.beRepaired(5);
-	// ClapTrap.setName("ClapTrap");
-	// std::cout << ClapTrap.getName() << std::endl;
+	ClapTrap.attack("target");
+	ClapTrap.takeDamage(10);
+	ClapTrap.beRepaired(5);
+	ClapTrap.setName("ClapTrap");
+	std::cout << ClapTrap.getName() << std::endl;
 
-	// ScavTrap.attack("target");
-	// ScavTrap.takeDamage(10);
-	// ScavTrap.beRepaired(5);
-	// ScavTrap.guardGate();
+	ScavTrap.attack("target");
+	ScavTrap.takeDamage(10);
+	ScavTrap.beRepaired(5);
+	ScavTrap.guardGate();
 
-	// FragTrap.attack("target");
-	// FragTrap.takeDamage(10);
-	// FragTrap.beRepaired(5);
-	// FragTrap.highFivesGuys();
+	FragTrap.attack("target");
+	FragTrap.takeDamage(10);
+	FragTrap.beRepaired(5);
+	FragTrap.highFivesGuys();
 
 	DiamondTrap.attack("target");
 	DiamondTrap.takeDamage(10);
